1413ddbd5fde8bd8f051ec4d7d0ecbfa6e3e60ad
Marcel Kloubert <marcel.kloubert@gmx.net>
2021-12-31 04:05:39 +0100

    fix caching

--- pages/api/news/[newsId]/image.ts
+++ pages/api/news/[newsId]/image.ts
@@ -45,7 +45,12 @@ const handler: NextApiHandler = async (
       });
 
       if (existingEntry?.image?.url?.length) {
-        const imageResponse = await fetch(existingEntry.image.url);
+        const imageResponse = await fetch(existingEntry.image.url, {
+          headers: {
+            // TODO: should be random and configurable
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1'
+          }
+        });
 
         if (imageResponse.status === 200) {
           const data = Buffer.from(await imageResponse.arrayBuffer());
